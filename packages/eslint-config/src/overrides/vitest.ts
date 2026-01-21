9a9a51fd91077f7e2504ffbe21adc426099c4b86
Chris Lawson <git@chrislawson.dev>
2026-01-20 22:59:20 -0800

    refactor: migrate to new eslint.config.mjs format, use rollup for bundling

--- packages/eslint-config/src/overrides/vitest.ts
+++
@@ -1,9 +0,0 @@
-import type { Linter } from '@typescript-eslint/utils/dist/ts-eslint';
-
-export const vitestOverrides: Linter.ConfigOverride = {
-  files: '*.spec.ts',
-  plugins: ['vitest'],
-  rules: {
-    '@typescript-eslint/dot-notation': 'off',
-  },
-};
